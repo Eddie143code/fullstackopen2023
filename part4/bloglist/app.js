fc5e6d83ac9f0a80d93148ca488f3c2872612173
Eddie <placenamehere1901@gmail.com>
2023-03-10 18:50:57 +0200

    part5 finished

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogRouter = require("./controllers/blog");
 const userRouter = require("./controllers/user");
+const testingRouter = require("./controllers/testing");
 const { getTokenFrom, userExtractor } = require("./utils/middleware");
 
 mongoose.connect(config.MONGODB_URI);
@@ -15,5 +16,10 @@ app.use(getTokenFrom);
 
 app.use("/api/blogs", userExtractor, blogRouter);
 app.use("/api/users", userRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 
 module.exports = app;
